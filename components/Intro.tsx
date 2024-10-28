7112216acc7005b34d1d2bbed3cf434eec84dbf3
yrtc99 <yrtc99@gmail.com>
2024-10-29 01:50:04 +0800

    “update”

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -5,7 +5,7 @@ export default function Intro() {
     return (
         <section className="min-h-screen flex flex-col items-center justify-center text-center p-4">
             <div className='flex flex-col w-full ml-8'>
-                <h2 className="text-2xl mb-2 bodoni-moda-regular w-full text-left  ">2025.01.15</h2>
+                <h2 className="text-2xl mb-2 bodoni-moda-regular w-full text-left  ">2025.01.25</h2>
                 <h1 className="text-2xl mb-4 bodoni-moda-regular w-full text-left">Save the Date</h1>
             </div>
             <Image src="/intro.jpg" alt="Eason & Cathy" width={350} height={400} className="rounded-lg shadow-lg mb-8" />
--- components/Overlay.tsx
+++ components/Overlay.tsx
@@ -1,13 +1,36 @@
-import React from 'react';
+"use client";
+
+import React, { useState } from 'react';
 
 interface OverlayProps {
     onClick: () => void;
 }
 
 const Overlay: React.FC<OverlayProps> = ({ onClick }) => {
+    const [isVisible, setIsVisible] = useState(true);
+
+    // 點擊觸發淡出效果
+    const handleClick = () => {
+        setIsVisible(false); // 觸發淡出效果
+        setTimeout(onClick, 500); // 在500ms後觸發外部的 onClick 行為（例如關閉 Overlay）
+    };
+
     return (
-        <div className="overlay" onClick={onClick}>
-            <h2>touch</h2>
+        <div
+            className={`overlay fixed inset-0 bg-white text-black flex items-center justify-center transition-opacity duration-500 ease-out ${isVisible ? 'opacity-100' : 'opacity-0'
+                }`}
+            onClick={handleClick}
+            style={{
+                backgroundImage: 'url(/oVERLAY.png)', // 確保背景圖片路徑正確
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                zIndex: 1000, // 確保遮罩在最上層
+            }}
+        >
+            {/* 忽明忽暗的箭頭 */}
+            <div className="absolute bottom-40 right-10 noto-serif-tc-regular tracking-wide text-black animate-pulse z-50 border border-black rounded-full px-4 py-2">
+                點擊進入
+            </div>
         </div>
     );
 };
