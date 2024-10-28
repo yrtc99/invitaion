69b33fefdd0e546614f461467ce6d52a98681d54
yrtc99 <yrtc99@gmail.com>
2024-10-28 23:23:23 +0800

    “update”

--- components/Brief3.tsx
+++ components/Brief3.tsx
@@ -4,7 +4,7 @@ export default function Brief3() {
     return (
         <section className="relative flex flex-col items-center justify-center"> {/* 設置父容器 relative */}
 
-            <Image src="/brief2text.png" alt="for you" width={300} height={300} className='absolute -top-8 -right-4 ' />
+            <Image src="/brief2names.png" alt="for you" width={300} height={300} className='absolute -top-8 -right-4 ' />
 
             <div className="w-full sm:w-[500px] sm:h-auto overflow-hidden mt-24 space-y-2 px-8 ">
                 <Image
